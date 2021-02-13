b2d6ef4beabd887d303454b57de85cebdcad8c1c
kernel503 <juanko682@gmail.com>
2021-02-12 22:05:34 -0600

    minor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    <script src="{{ asset(js/app.js)}}" defer></script>
+    <script src="{{ asset('js/app.js')}}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -21,7 +21,7 @@
 
     <!-- Styles -->
      {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    <link href="{{ asset(css/app.css)}}" rel="stylesheet">
+    <link href="{{ asset('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
