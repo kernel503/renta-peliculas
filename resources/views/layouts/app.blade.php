abac15cafce3168e259cb443e467fad5214540bd
kernel503 <juanko682@gmail.com>
2021-02-12 21:59:49 -0600

    assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    <script src="js/app.js" defer></script>
+    <script src="{{ asset(js/app.js)}}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,7 +20,8 @@
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="css/app.css" rel="stylesheet">
+     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+    <link href="{{ asset(css/app.css)}}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
